06cba363c5f5e1238a3d67c1004dcc28bfb14933
David Owino <daveowino753@gmail.com>
2024-03-12 12:50:47 -0700

    added file 7

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Author - Stephen Musyoka Makola
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Author - Stephen Musyoka Makola
 for number in range(0, 99):
     print("{} = {}".format(number, hex(number)))
     
